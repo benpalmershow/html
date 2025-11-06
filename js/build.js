784196745b28fa8a31f35cab667424a51eeb3205
benpalmershow <Benjamin.r.palmer@gmail.com>
2025-11-05 18:28:43 -0800

    SEO fix

--- js/build.js
+++ js/build.js
@@ -1,24 +1,59 @@
 import { build } from "bun";
 
+// Build JavaScript files
 await build({
-  entrypoints: [
-    "./js/countdown.js",
-    "./js/portfolio.js",
-    "./js/nav.js",
-    "./js/dark-mode.js",
-    "./js/financials.js"
-  ],
-  outdir: "./dist",
+entrypoints: [
+"./js/countdown.js",
+"./js/portfolio.js",
+"./js/nav.js",
+"./js/dark-mode.js",
+  "./js/financials.js",
+  "./js/news.js",
+  "./js/media.js",
+"./js/journal-feed.js",
+"./js/post-feed.js",
+"./js/posts-loader.js",
+  "./js/socials.js",
+  "./js/animations.js",
+  "./js/markdown-parser.js",
+  "./js/news-preview.js"
+],
+outdir: "./dist/js",
   minify: {
     whitespace: true,
     identifiers: true,
     syntax: true
   },
-  naming: "[dir]/[name].[hash].[ext]",
+  naming: "[name].[hash].[ext]",
   sourcemap: "external",
   target: "browser",
   splitting: true,
   plugins: []
 });
 
-console.log("Build completed! Files output to ./html/dist");
+// Build CSS files
+await build({
+  entrypoints: [
+    "./css/body.css",
+    "./css/dark-mode.css",
+    "./css/page-titles.css",
+    "./css/index.css",
+    "./css/news.css",
+    "./css/journal-tweets.css",
+    "./css/media.css",
+    "./css/financials.css",
+    "./css/read.css",
+    "./css/chart-modal.css",
+    "./css/newspaper-titles.css"
+  ],
+  outdir: "./dist/css",
+  minify: {
+    whitespace: true,
+    identifiers: true,
+    syntax: true
+  },
+  naming: "[name].[hash].[ext]",
+  target: "browser"
+});
+
+console.log("Build completed! Files output to ./dist");
